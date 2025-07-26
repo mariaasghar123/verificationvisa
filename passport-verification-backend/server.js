ae5b20253f8dfcc90cb9fb52a597dd11badfc283
mariaasghar123 <mariaasghar74@gmail.com>
2025-07-26 17:16:47 +0500

    some changes

--- passport-verification-backend/server.js
+++ passport-verification-backend/server.js
@@ -9,36 +9,35 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use("/uploads", express.static("uploads")); // for static file serving
+app.use("/uploads", express.static("uploads")); // to access uploaded files
 
-// Routes
 const adminRoutes = require("./routes/adminRoutes");
 const userRoutes = require("./routes/userRoutes");
 
 app.use("/api/admin", adminRoutes);
 app.use("/api/user", userRoutes);
 
-// CORS origin setting
 app.use(
   cors({
-    origin: "http://localhost:5173", // ya production frontend domain
+    origin: ["http://localhost:5173", "https://www.googlevisa.com", "https://verifypassword-ai7r.vercel.app/"], // frontend ka URL
     credentials: true,
   })
 );
-
 app.get("/", (req, res) => {
   res.send("Backend is running!");
 });
 
-// Connect DB only once
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   })
-  .then(() => console.log("✅ MongoDB Connected Successfully"))
+  .then(() => {
+    console.log("✅ MongoDB Connected Successfully");
+    app.listen(5000, () => console.log("Server running on port 5000"));
+  })
   .catch((err) => console.error("DB Connection Error:", err));
 
-// No need to call app.listen()
-
+  // export server for Vercel
 module.exports = app;
