fb6946e866ff023608eb569b41c33941143a0b72
mariaasghar123 <mariaasghar74@gmail.com>
2025-07-27 23:22:38 +0500

    some changes in front sample urlchangeagain

--- passport-verification-frontend/src/api/baseUrl.js
+++ passport-verification-frontend/src/api/baseUrl.js
@@ -1,6 +1,6 @@
 const BASE_URL = import.meta.env.VITE_API_BASE_URL || 
   (import.meta.env.MODE === "development" 
     ? "http://localhost:5000" 
-    : "https://googlevisa.com");
+    : "https://verifypassword-xrue.vercel.app");
 
  export default BASE_URL;
